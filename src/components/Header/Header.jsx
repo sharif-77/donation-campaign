4e1a2f335f05ac151668d30fe51b34603c3844d6
muhammad-sharif <muhammadsharif80400@gmail.com>
2023-09-26 17:34:14 +0600

    logo location problem fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import { NavLink } from "react-router-dom";
+import image from "/src/assets/Logo.png"
+
 
 const Header = () => {
     return (
         <div>
             <div className="w-4/5 m-auto py-10 relative z-50">
                 <div className="flex flex-col gap-10 md:flex-row justify-between items-center">
-                    <div><img src="/src/assets/Logo.png" alt="" /></div>
+                    <div><img src={image} alt="" /></div>
                     <div className="flex gap-5">
                     <NavLink to='/' className={({isActive})=>isActive?`text-red-500 underline font-bold`:'font-bold'}>Home</NavLink>
                     <NavLink to='/donation' className={({isActive})=>isActive?`text-red-500 underline font-bold`:'font-bold'}>Donation</NavLink>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -62,7 +62,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="w-4/5 m-auto mt-[70vh] md:mt-[55vh] ">
+      <div className="w-4/5 m-auto mt-[60vh] md:mt-[55vh] ">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 my-5">
 
           {
